700fc4578ae28535dfabd71b13ca6e92442a47b2
Regis Digital <regissdigital@gmail.com>
2026-06-07 05:20:53 +0530

    Completely overhaul pyproject.toml and setup.py to fix bench get-app installation crash

--- setup.py
+++ setup.py
@@ -1,19 +1,13 @@
 from setuptools import setup, find_packages
 
-with open("requirements.txt") as f:
-	install_requires = [line.strip() for line in f.read().splitlines() if line.strip()]
-
-# get version from __version__ variable in tms/__init__.py
-from tms import __version__ as version
-
 setup(
 	name="tms",
-	version=version,
+	version="0.0.1",
 	description="Transportation Management System",
 	author="NexGen ERP Technologies",
 	author_email="admin@example.com",
 	packages=find_packages(),
 	zip_safe=False,
 	include_package_data=True,
-	install_requires=install_requires
+	install_requires=[]
 )
